d3432ac25b1d625ab4c85c8c502cd1903a32bc32
34255skn-tw <34255.skn@skn.ac.th>
2026-07-07 22:45:26 +0700

    Update mediapipe_pose.py

--- pose/mediapipe_pose.py
+++ pose/mediapipe_pose.py
@@ -1,5 +1,6 @@
 import mediapipe as mp
-mp_pose = mp.solutions.pose
+# เปลี่ยนวิธีดึงข้อมูลย่อยเป็นแบบเรียกตรง เพื่อรองรับ MediaPipe ทุกเวอร์ชัน
+from mediapipe.python.solutions import pose as mp_pose
 
 def create_pose():
     return mp_pose.Pose(
