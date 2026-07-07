108f051646a6323c0aa22396e5bae94cf01a40f8
34255skn-tw <34255.skn@skn.ac.th>
2026-07-07 22:25:34 +0700

    Update mediapipe_pose.py

--- pose/mediapipe_pose.py
+++ pose/mediapipe_pose.py
@@ -1,10 +1,7 @@
 import mediapipe as mp
-
-# แนะนำให้เรียกใช้ผ่านการอ้างอิงตรงจากโมดูลย่อย เพื่อรองรับ MediaPipe เวอร์ชันใหม่ๆ
-from mediapipe.python.solutions import pose as mp_pose
+mp_pose = mp.solutions.pose
 
 def create_pose():
-    # เรียกใช้งาน Pose Instance ตามโครงสร้างเดิมของคุณ
     return mp_pose.Pose(
         static_image_mode=False,
         model_complexity=1,
